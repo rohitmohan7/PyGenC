2f3c9bab25b90450fde4757d7af69be6e00f2e80
unknown <rohitmohan7@gmail.com>
2020-02-02 22:30:39 -0500

    update

---
+++ base_repo/CMakeFiles/executeTests.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/PyGenC/base_repo/src/MyString.cpp" "/mnt/d/PyGenC/base_repo/CMakeFiles/executeTests.dir/src/MyString.cpp.o"
+  "/mnt/d/PyGenC/base_repo/tests/MyString_test.cpp" "/mnt/d/PyGenC/base_repo/CMakeFiles/executeTests.dir/tests/MyString_test.cpp.o"
+  "/mnt/d/PyGenC/base_repo/tests/test_main.cpp" "/mnt/d/PyGenC/base_repo/CMakeFiles/executeTests.dir/tests/test_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ base_repo/tests/MyString_test.cpp
@@ -0,0 +1,39 @@
+
+/**************** Auto Generated File **********************/
+
+#include "../includes/MyString.h"
+#include <gtest/gtest.h>
+
+const char kHelloString[] = "Hello, world!";
+
+TEST(MyString, DefaultConstructor) {
+  MyString s;
+  EXPECT_STREQ(NULL, s.c_string());
+  EXPECT_EQ(0u, s.Length());
+}
+
+TEST(MyString, Constructor_kHelloString) {
+  MyString s(kHelloString);
+  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
+  EXPECT_EQ(sizeof(kHelloString)/sizeof(kHelloString[0]) - 1,s.Length());
+}
+
+TEST(MyString, CopyConstructor) {
+  MyString s1(kHelloString);
+  MyString s2 = s1;
+  EXPECT_EQ(0, strcmp(s2.c_string(), kHelloString));
+}
+
+TEST(MyString, Set) {
+  MyString s;
+
+  s.Set(kHelloString);
+  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
+
+  s.Set(s.c_string());
+  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
+
+  s.Set(NULL);
+  EXPECT_STREQ(NULL, s.c_string());
+}
+
